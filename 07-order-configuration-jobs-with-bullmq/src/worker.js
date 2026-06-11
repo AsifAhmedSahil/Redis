36e21348fc4aeaef01e1696e4391d0bd7803f878
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-06-11 09:43:28 +0600

    BullMQ Queue infrastructure with redis

---
+++ 07-order-configuration-jobs-with-bullmq/src/worker.js
@@ -0,0 +1,24 @@
+import {Worker} from "bullmq"
+import {connection} from "./queue.js"
+
+
+const worker = new Worker(
+    "emails",
+    async(job)=>{
+        console.log('processing email job...',job.id,job.name,job.data);
+        (await new Promise((resolve)=> setTimeout(resolve,1500)),
+        console.log("Email job completed:",job.id,job.name,job.data));
+    },
+    {
+        connection
+    }
+)
+
+worker.on("completed",(job)=>{
+    console.log("job completed!",job.id,job.name,job.data);
+});
+
+worker.on("failed",(job,err)=>{
+    console.log("job failed!",job.id,job.name,job.data,err);
+});
+
